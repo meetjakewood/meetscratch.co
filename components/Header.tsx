8f7c03e7ff0348f22ac690dabf92fbadbaa0b501
Jake Wood <meetjakewood@users.noreply.github.com>
2024-10-14 22:23:06 +1000

    Fix error with empty navigation list, handle in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,19 +29,21 @@ const Header = () => {
         </div>
       </Link>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
-          {headerNavLinks
-            .filter((link) => link.href !== '/')
-            .map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-              >
-                {link.title}
-              </Link>
-            ))}
-        </div>
+        {headerNavLinks.length > 0 && (
+          <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
+            {headerNavLinks
+              .filter((link) => link.href !== '/')
+              .map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                >
+                  {link.title}
+                </Link>
+              ))}
+          </div>
+        )}
         <SearchButton />
         <ThemeSwitch />
         <MobileNav />
